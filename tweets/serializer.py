3347de1a10ade22d4a9b6079de222af445bf9a1e
rogerchanac007 <armandoroger007@gmail.com>
2022-10-07 03:12:53 +0000

    User display serializer

--- tweets/serializer.py
+++ tweets/serializer.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Tweet
 
+User = get_user_model()
+
+class UserDisplaySerializer(serializers.ModelSerializer):
+    follower_count = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'follower_count']
+    
+    def get_follower_count(self, obj):
+        return 0
+
 class TweetSerializer(serializers.ModelSerializer):
+    user = UserDisplaySerializer()
     class Meta:
         model = Tweet
-        fields = '__all__'
+        fields = ['user', 'content']
